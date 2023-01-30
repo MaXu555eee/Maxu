a60602fb968fe0a5b93f1d1353ffb28c46d85e4f
DESKTOP-1EHJCK4\admin <ma514542595@hotmail.com>
2023-01-30 19:57:18 +0800

    no message

---
+++ Maxx.java
@@ -0,0 +1,18 @@
+public class Maxx {
+	
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 10;
+		int c = 268;
+		int d = 2;
+		System.out.println("a + b =" + (a + b));
+		System.out.println("a - b =" + (a - b));
+		System.out.println("a * b =" + (a * b));
+		System.out.println("b / a =" + (b / a));
+		System.out.println("a + c =" + (a + c));
+		System.out.println("c + b =" + (c + b));
+		System.out.println("++d =" + (++d));
+		System.out.println("--d =" + (--d));
+		System.out.println("b % a =" + (b % a));
+	}
+}
---
+++ TestifElse.java
@@ -0,0 +1,14 @@
+public class TestifElse {
+	public static void main(String[] args) {
+		int x = 20;
+		if(x < 10) {
+			System.out.println("天王盖地虎");
+		} else {
+			System.out.println("宝塔镇河妖");
+			
+		}
+		if(x < 20) {
+			System.out.println("天王盖地虎");
+		}
+	}
+}
